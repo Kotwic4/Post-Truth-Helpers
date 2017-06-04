2e1cc8b988cd415a9910e5cf0bd24a3559b6871d
Radomir Krawczykiewicz <kotwic4@gmail.com>
2017-06-04 15:06:36 +0200

    update project structure

---
+++ post_truth_helpers/__init__.py
@@ -0,0 +1,5 @@
+import post_truth_helpers.currency_validation as currency_validation
+
+__all__ = [
+    'currency_validation'
+]
--- post_truth_helpers/currency_validation.py
+++ post_truth_helpers/currency_validation.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import urllib.request
 import json
 
